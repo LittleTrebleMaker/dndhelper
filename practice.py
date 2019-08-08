af86b4fe08ecba7056d8d6d4dde6bcce0dd8aa3c
A_La_Code <kathleen.clift@gmail.com>
2019-08-08 19:40:49 -0400

    Create practice.py

---
+++ practice.py
@@ -0,0 +1,18 @@
+#comments look like this
+
+characterName = "Dilys";
+
+#indentation is important. See below. 
+
+if 5 > 2 :
+  print('true')
+  
+#multiline strings can be indicated by three double or three single quotes. 
+
+print("""Double Butts""")
+print('''Single Butts''')
+
+#arrays look the same as javascript. 
+thisList = ["cuts","butts","coconuts"]
+print(thisList[1]) #Butts
+
